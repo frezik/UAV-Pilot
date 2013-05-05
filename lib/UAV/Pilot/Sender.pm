d9627c8d333873d05b02678812e02458cd5ab567
tmurray <tmurray@wumpus-cave.net>
2013-05-05 10:32:04 -0500

    POD doc for UAV::Pilot::Sender

--- lib/UAV/Pilot/Sender.pm
+++ lib/UAV/Pilot/Sender.pm
@@ -9,3 +9,13 @@ __PACKAGE__->meta->make_immutable;
 1;
 __END__
 
+
+=head1 NAME
+
+  UAV::Pilot::Sender
+
+=head1 DESCRIPTION
+
+This is an abstract class for a low-level interface to a given UAV.  These are primarily for 
+those developing the C<UAV::Pilot> API against a new UAV.  Programmers seeking to use an 
+existing UAV should look at L<UAV::Pilot::Sender>.
