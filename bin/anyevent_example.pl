564f79ac4ec2bdc405804020861933593b2a58de
tmurray <tmurray@wumpus-cave.net>
2013-05-05 16:35:27 -0500

    Demo of AnyEvent usage.  Needs tweeking.

---
+++ bin/anyevent_example.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+use v5.14;
+use strict;
+use warnings;
+
+use AnyEvent;
+use UAV::Pilot;
+use UAV::Pilot::Sender::ARDrone;
+use UAV::Pilot::Device::ARDrone;
+
+# According to SDK docs, smooth flight needs commands sent every 30ms
+use constant INT => 1 / 30;
+use constant IP  => '192.168.1.1';
+
+
+my $cv = AE::cv;
+
+my $ardrone = UAV::Pilot::Sender::ARDrone->new({
+    host => IP,
+});
+$ardrone->connect;
+my $ar = UAV::Pilot::Device::ARDrone->new({
+    sender => $ardrone,
+});
+
+$ar->takeoff;
+
+my $t = AE::timer( 5, INT, sub {
+    $ar->pitch( -0.5 );
+});
+
+AE::timer( 6, 0, sub {
+    undef $t;
+    my $t2 = AE::timer( 0, INT, sub {
+        $ar->pitch( 0.5 );
+    });
+
+    AE::timer( 2, INT, sub {
+        undef $t2;
+        $ar->land;
+        $cv->send; # End main event loop
+        exit;
+    });
+});
+
+$cv->recv; # Enter main event loop
