5a9bc312ffacf93525efd1e35e861eeaccf9d362
tmurray <tmurray@wumpus-cave.net>
2013-06-14 21:02:34 -0500

    Document EasyEvent

--- lib/UAV/Pilot/EasyEvent.pm
+++ lib/UAV/Pilot/EasyEvent.pm
@@ -82,3 +82,105 @@ __PACKAGE__->meta->make_immutable;
 1;
 __END__
 
+
+=head1 NAME
+
+  UAV::Pilot::EasyEvent
+
+=head1 SYNOPSIS
+
+    my $cv = AnyEvent->condvar;
+    my $event = UAV::Pilot::EasyEvent->new({
+        condvar => $cv,
+    });
+    
+    my @events;
+    my $event2 = $event->add_timer({
+        duration       => 100,
+        duration_units => $event->UNITS_MILLISECOND,
+        cb             => sub {
+            push @events => 'First event',
+        },
+    })->add_timer({
+        duration       => 10,
+        duration_units => $event->UNITS_MILLISECOND,
+        cb             => sub {
+            push @events => 'Second event',
+        },
+    });
+    
+    $event2->add_timer({
+        duration       => 50,
+        duration_units => $event->UNITS_MILLISECOND,
+        cb             => sub {
+            push @events => 'Fourth event',
+            $cv->send;
+        },
+    });
+    $event2->add_timer({
+        duration       => 10,
+        duration_units => $event->UNITS_MILLISECOND,
+        cb             => sub {
+            push @events => 'Third event',
+        },
+    });
+    
+    
+    $event->activate_events;
+    $cv->recv;
+    
+    # After time passes, prints:
+    # First event
+    # Second event
+    # Third event
+    # Fourth event
+    #
+    say $_ for @events;
+
+=head1 DESCRIPTION
+
+C<AnyEvent> is the standard event framework used for C<UAV::Pilot>.  However, its 
+interface isn't convenient for some of the typical things done for UAV piloting.  For 
+instance, to put the code into plain English, we might want to say:
+
+    Takeoff, wait 5 seconds, then pitch forward for 2 seconds, then pitch backwards 
+    for 2 seconds, then land
+
+In the usual C<AnyEvent> interface, this requires building the timers inside the callbacks 
+of other timers, which leads to several levels of indentation.  C<UAV::Pilot::EasyEvent> 
+simplifies the handling and syntax of this kind of event workflow.
+
+=head1 METHODS
+
+=head2 new
+
+    new({
+        condvar => $cv,
+    })
+
+Constructor.  The C<condvar> argument should be an C<AnyEvent::CondVar>.
+
+=head2 add_timer
+
+    add_timer({
+        duration       => 100,
+        duration_units => $event->UNITS_MILLISECOND,
+        cb             => sub { ... },
+    })
+
+Add a timer to run in the event loop.  It will run after C<duration> units of time, with 
+the units specified by C<duration_units>.  The C<cb> parameter is a reference to a 
+subroutine to use as a callback.
+
+Returns a child C<EasyEvent> object.  When the timer above has finished, any timers on 
+child objects will be setup for execution.  This makes it easy to chain timers to run 
+after each other.
+
+=head2 activate_events
+
+This method must be called after running a series of C<add_timer()> calls.  You only need 
+to call this on the root object, not the children.
+
+You must call C<recv> on the C<condvar> yourself.
+
+=cut
