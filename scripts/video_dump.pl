ba2f3db3543f4f6964838fb7c03035a0c9ba33b7
tmurray <tmurray@wumpus-cave.net>
2013-06-19 14:58:13 -0500

    Start on dumping of h264 payload

--- scripts/video_dump.pl
+++ scripts/video_dump.pl
@@ -6,45 +6,83 @@ use Data::Dumper 'Dumper';
 
 local $Data::Dumper::Sortkeys = 1;
 
+use constant PAVE_SIGNATURE => 'PaVE';
+use constant BUF_READ_SIZE  => 4096;
+
 my $HOST = '192.168.1.1';
 my $PORT = 5555;
 my $FILE = shift;
 
 
-sub parse_header
+sub read_frame
 {
-    my ($packet) = @_;
-    my %header;
-    my @bytes = unpack "C*", $packet;
+    my ($input) = @_;
+    my $buf;
+    my $got_input = $input->read( $buf, BUF_READ_SIZE );
+    return {}, 0 if ! $got_input;
 
-    $header{signature}               = pack 'c4', @bytes[0..3];
-    $header{version}                 = $bytes[4];
-    $header{video_codec}             = $bytes[5];
-    $header{header_size}             = convert_16bit_LE( @bytes[6,7]);
-    $header{payload_size}            = convert_32bit_LE( @bytes[8..11] );
-    $header{encoded_stream_width}    = convert_16bit_LE( @bytes[12,13] );
-    $header{encoded_stream_height}   = convert_16bit_LE( @bytes[14,15] ); # here
-    $header{display_width}           = convert_16bit_LE( @bytes[16,17] );
-    $header{display_height}          = convert_16bit_LE( @bytes[18,19] );
-    $header{frame_number}            = convert_32bit_LE( @bytes[20..23] );
-    $header{timestamp}               = convert_32bit_LE( @bytes[23..26] );
-    $header{total_chunks}            = $bytes[27];
-    $header{chunk_index}             = $bytes[28];
-    $header{frame_type}              = pack 'C', $bytes[29];
-    $header{control}                 = $bytes[30];
-    $header{stream_byte_position_lw} = convert_32bit_LE( @bytes[31..34] );
-    $header{stream_byte_position_uw} = convert_32bit_LE( @bytes[35..38] );
-    $header{stream_id}               = convert_16bit_LE( @bytes[39,40] );
-    $header{total_slices}            = $bytes[41];
-    $header{slice_index}             = $bytes[42];
-    $header{header1_size}            = $bytes[43];
-    $header{header2_size}            = $bytes[44];
-    $header{reserved2}               = pack 'C2', @bytes[45,46];
-    $header{advertised_size}         = convert_32bit_LE( @bytes[47..50] );
-    $header{reserved3}               = pack 'C12', @bytes[51..62];
+    my @bytes = unpack "C*", $buf;
 
-    return \%header;
+    my %packet;
+    $packet{signature}               = pack 'c4', @bytes[0..3];
+    $packet{version}                 = $bytes[4];
+    $packet{video_codec}             = $bytes[5];
+    $packet{packet_size}             = convert_16bit_LE( @bytes[6,7]);
+    $packet{payload_size}            = convert_32bit_LE( @bytes[8..11] );
+    $packet{encoded_stream_width}    = convert_16bit_LE( @bytes[12,13] );
+    $packet{encoded_stream_height}   = convert_16bit_LE( @bytes[14,15] ); # here
+    $packet{display_width}           = convert_16bit_LE( @bytes[16,17] );
+    $packet{display_height}          = convert_16bit_LE( @bytes[18,19] );
+    $packet{frame_number}            = convert_32bit_LE( @bytes[20..23] );
+    $packet{timestamp}               = convert_32bit_LE( @bytes[23..26] );
+    $packet{total_chunks}            = $bytes[27];
+    $packet{chunk_index}             = $bytes[28];
+    $packet{frame_type}              = pack 'C', $bytes[29];
+    $packet{control}                 = $bytes[30];
+    $packet{stream_byte_position_lw} = convert_32bit_LE( @bytes[31..34] );
+    $packet{stream_byte_position_uw} = convert_32bit_LE( @bytes[35..38] );
+    $packet{stream_id}               = convert_16bit_LE( @bytes[39,40] );
+    $packet{total_slices}            = $bytes[41];
+    $packet{slice_index}             = $bytes[42];
+    $packet{packet1_size}            = $bytes[43];
+    $packet{packet2_size}            = $bytes[44];
+    $packet{reserved2}               = pack 'C2', @bytes[45,46];
+    $packet{advertised_size}         = convert_32bit_LE( @bytes[47..50] );
+    $packet{reserved3}               = pack 'C12', @bytes[51..62];
+    warn "Bad PaVE signature, got: '$packet{signature}'\n"
+        if PAVE_SIGNATURE ne $packet{signature};
+
+    my ($payload, $continue_reading) = read_frame_payload(
+        [@bytes[63..$#bytes]] ,
+        $input,
+        $packet{payload_size}
+    );
+    $packet{payload} = $$payload;
+    return (\%packet, $continue_reading);
 }
+
+sub read_frame_payload
+{
+    my ($leftover_bytes, $input, $total_size) = @_;
+    my @bytes = @{ $leftover_bytes };
+    my $out = join '', @bytes;
+
+    my $continue = 1;
+    while( (length($out) < $total_size) && $continue ) {
+        my $buf;
+        my $size_left = $total_size - scalar(@bytes);
+        my $buf_size = ($size_left > BUF_READ_SIZE)
+            ? BUF_READ_SIZE
+            : $size_left;
+        my $bytes_recv = $input->read( $buf, $buf_size );
+        $continue = 0 if ! $bytes_recv;
+
+        $out .= $buf;
+    }
+
+    return (\$out, $continue);
+}
+
 
 sub convert_32bit_LE
 {
@@ -77,15 +115,13 @@ sub convert_16bit_LE
         ) or die "Can't open socket to $HOST:$PORT: $!\n";
     }
 
-    my $buf = '';
-    my $in = $input->read( $buf, 4096 );
-    if( $in ) {
-        my $header = parse_header( $buf );
-        print Dumper( $header );
+    my $continue = 1;
+    while( $continue ) {
+        my $frame = {};
+        ($frame, $continue) = read_frame( $input );
+        print $$frame{payload} if %$frame;
     }
-    else {
-        say "Didn't get a packet\n";
-    }
+
 
     $input->close;
 }
