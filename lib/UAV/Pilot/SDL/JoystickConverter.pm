47bee0c2a82184679fde487aa3ccf44d1b1f8e55
tmurray <tmurray@wumpus-cave.net>
2013-06-29 13:55:10 -0500

    Add documentation

--- lib/UAV/Pilot/SDL/JoystickConverter.pm
+++ lib/UAV/Pilot/SDL/JoystickConverter.pm
@@ -12,3 +12,27 @@ requires 'convert_sdl_input';
 1;
 __END__
 
+
+=head1 NAME
+
+  UAV::Pilot::SDL::JoystickConverter
+
+=head1 DESCRIPTION
+
+Role for objects that would take axis data from the SDL joystick input and convert it into 
+the numbers needed for their specific driver.
+
+=head1 CONSTANTS
+
+=head2 MAX_AXIS_INT
+
+=head2 MIN_AXIS_INT
+
+=head1 REQUIRED METHODS
+
+=head2 convert_sdl_input
+
+Will be passed an SDL joystick value between C<MAX_AXIS_INT> and C<MIN_AXIS_INT>.  Should 
+return an equivilent value for your UAV.
+
+=cut
