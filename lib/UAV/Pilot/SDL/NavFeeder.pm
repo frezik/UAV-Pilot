14b8a1f9eb12e17cc9abb5ea51e50048111be406
tmurray <tmurray@wumpus-cave.net>
2013-06-29 13:50:45 -0500

    Add documentation

--- lib/UAV/Pilot/SDL/NavFeeder.pm
+++ lib/UAV/Pilot/SDL/NavFeeder.pm
@@ -11,3 +11,25 @@ requires 'cur_vert_speed';
 
 1;
 __END__
+
+
+=head1 NAME
+
+  UAV::Pilot::SDL::NavFeeder
+
+=head1 DESCRIPTION
+
+Role for objects that can provide data to navigation displays, such as 
+C<UAV::Pilot::Control::SDLNavOutput>.
+
+=head1 METHODS
+
+=head2 cur_pitch
+
+=head2 cur_roll
+
+=head2 cur_yaw
+
+=head2 cur_vert_speed
+
+=cut
