0221199a6902e7d7d6ffd45b03a2ebd2f78b1491
Timm Murray <tmurray@wumpus-cave.net>
2013-10-26 23:54:14 +0000

    Error handling

--- lib/UAV/Pilot/WumpusRover/Server.pm
+++ lib/UAV/Pilot/WumpusRover/Server.pm
@@ -22,6 +22,10 @@ has '_socket' => (
     is  => 'rw',
     isa => 'Maybe[IO::Socket::INET]',
 );
+has '_condvar' => (
+    is  => 'rw',
+    isa => 'Maybe[AnyEvent::CondVar]',
+);
 with 'UAV::Pilot::Server';
 with 'UAV::Pilot::Logger';
 
@@ -29,8 +33,7 @@ with 'UAV::Pilot::Logger';
 sub init_listen_events
 {
     my ($self, $cv) = @_;
-    my $logger = $self->_logger;
-    $logger->info( 'Starting listener on UDP port '
+    $self->_logger->info( 'Starting listener on UDP port '
         . $self->listen_port );
 
     my $socket = IO::Socket::INET->new(
@@ -46,26 +49,13 @@ sub init_listen_events
         fh   => $socket,
         poll => 'r',
         cb   => sub {
-            $logger->info( 'Received packet' );
-
-            my $buf = undef;
-            if( my $len = $socket->read( \$buf, $self->BUF_LENGTH ) ) {
-                $logger->info( "Read $len bytes" );
-                my $packet = UAV::Pilot::WumpusRover::PacketFactory
-                    ->read_packet( $buf );
-                $logger->info( 'Processing message ID: '
-                    . $packet->message_id . ' (type: ' . ref($packet) . ')' );
-                $self->process_packet( $packet );
-            }
-            else {
-                $logger->info( "No data to read" );
-            }
-
+            $self->_read_packet;
             $event;
         },
     );
 
-    $logger->info( 'Done starting listener' );
+    $self->_condvar( $cv );
+    $self->_logger->info( 'Done starting listener' );
     return 1;
 }
 
@@ -76,6 +66,61 @@ sub process_packet
     if( $self->backend->process_packet( $packet ) ) {
         my $ack = $self->_build_ack_packet( $packet );
         $self->_send_packet( $ack );
+    }
+
+    return 1;
+}
+
+sub _read_packet
+{
+    my ($self) = @_;
+    my $logger = $self->_logger;
+    $logger->info( 'Received packet' );
+
+    my $buf = undef;
+    my $len = read( $self->_socket, $buf, $self->BUF_LENGTH );
+    if( defined($len) && ($len > 0) ) {
+        my $len = length $buf;
+        $logger->info( "Read $len bytes" );
+        my $packet = eval {
+            my $packet = UAV::Pilot::WumpusRover::PacketFactory
+                ->read_packet( $buf );
+            $logger->info( 'Processing message ID: '
+                . $packet->message_id . ' (type: ' . ref($packet) . ')' );
+            $self->process_packet( $packet );
+        };
+        if( ref($@) ) {
+            if( $@->isa( 'UAV::Pilot::ArdupilotPacketException::Badheader' ) ) {
+                $self->_logger->warn(
+                    'Bad header in packet: [' . $@->got_header . ']' );
+            }
+            elsif( $@->isa(
+                'UAV::Pilot::ArdupilotPacketException::BadChecksum'
+            )) {
+                $self->_logger->warn( 'Bad checksum in packet' );
+                $self->_logger->warn( 'Expected checksum: '
+                    . $@->expected_checksum1 . ', ' . $@->expected_checksum2 );
+                $self->_logger->warn( 'Got checksum: '
+                    . $@->got_checksum1 . ', ' . $@->got_checksum2 );
+            }
+            else {
+                $self->_logger->warn( 'Got exception: ' . ref($@) );
+                $@->rethrow;
+            }
+        }
+        elsif( $@ ) {
+            die "Error processing packet: $@\n";
+        }
+    }
+    elsif(! defined $len) {
+        # Error
+        UAV::Pilot::IOException->throw({
+            error => $!,
+        });
+    }
+    else {
+        # Shouldn't happen with AnyEvent.  Probably.
+        $logger->info( "No data to read" );
     }
 
     return 1;
