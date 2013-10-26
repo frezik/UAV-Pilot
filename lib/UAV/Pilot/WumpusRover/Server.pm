2974fe244d3d60a67a0a78b0fb9b9bd755782e4b
tmurray <tmurray@wumpus-cave.net>
2013-10-26 12:09:41 -0500

    Listen on port

--- lib/UAV/Pilot/WumpusRover/Server.pm
+++ lib/UAV/Pilot/WumpusRover/Server.pm
@@ -2,18 +2,71 @@ package UAV::Pilot::WumpusRover::Server;
 use v5.14;
 use Moose;
 use namespace::autoclean;
+use UAV::Pilot::WumpusRover::PacketFactory;
+
+use constant BUF_LENGTH => 1024;
 
 
 has 'listen_port' => (
-    is  => 'ro',
-    isa => 'Int',
+    is      => 'ro',
+    isa     => 'Int',
+    default => 49000,
 );
 has 'backend' => (
     is  => 'ro',
     isa => 'UAV::Pilot::WumpusRover::Server::Backend',
 );
+has '_socket' => (
+    is  => 'rw',
+    isa => 'Maybe[IO::Socket::INET]',
+);
 with 'UAV::Pilot::Server';
+with 'UAV::Pilot::Logger';
 
+
+sub init_listen_events
+{
+    my ($self, $cv) = @_;
+    my $logger = $self->_logger;
+    $logger->info( 'Starting listener on UDP port '
+        . $self->listen_port );
+
+    my $socket = IO::Socket::INET->new(
+        Proto     => 'udp',
+        LocalPort => $self->listen_port,
+        Blocking  => 0,
+        Listen    => 5,
+    ) or UAV::Pilot::IOException->throw(
+        error => 'Could not open socket: ' . $!,
+    );
+    $self->_socket( $socket );
+
+    my $event; $event = AnyEvent->io(
+        fh   => $socket,
+        poll => 'r',
+        cb   => sub {
+            $logger->info( 'Received packet' );
+
+            my $buf = undef;
+            if( my $len = $socket->read( \$buf, $self->BUF_LENGTH ) ) {
+                $logger->info( "Read $len bytes" );
+                my $packet = UAV::Pilot::WumpusRover::PacketFactory
+                    ->read_packet( $buf );
+                $logger->info( 'Processing message ID: '
+                    . $packet->message_id . ' (type: ' . ref($packet) . ')' );
+                $self->process_packet( $packet );
+            }
+            else {
+                $logger->info( "No data to read" );
+            }
+
+            $event;
+        },
+    );
+
+    $logger->info( 'Done starting listener' );
+    return 1;
+}
 
 sub process_packet
 {
